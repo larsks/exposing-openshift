567b7c188c7fe08f8ccce145e22f184d27737d58
Lars Kellogg-Stedman <lars@redhat.com>
2023-10-17 14:54:16 -0400

    Add :z option to bind mounts for selinux
    The :z option causes podman to relabel files and directories so that access
    from inside the container will be permitted by selinux. Without this
    change it is likely you would encounter mysterious "permission denied"
    issues on an selinux-enabled system.

--- using-haproxy-http/run.sh
+++ using-haproxy-http/run.sh
@@ -3,6 +3,6 @@
 [[ -f server.crt ]] || sh makecert.sh
 
 exec podman run -d --rm --name haproxy \
-	-v $PWD/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg \
-	-v $PWD:/certs \
+	-v $PWD/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:z \
+	-v $PWD:/certs:z \
 	-p 6443:6443 -p 80:8080 -p 443:8443 docker.io/haproxy:2.8
--- using-haproxy-tcp/run.sh
+++ using-haproxy-tcp/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 exec podman run -d --rm --name haproxy \
-	-v $PWD/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg \
+	-v $PWD/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:z \
 	-p 6443:6443 -p 80:8080 -p 443:8443 docker.io/haproxy:2.8
